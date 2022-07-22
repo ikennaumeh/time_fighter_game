ace4ed4841e562ec7941d816f3856807380bccfc
Ikenna Umeh <kennaumeh@gmail.com>
2022-07-22 10:32:15 +0100

    updates

--- app/src/main/java/com/example/timefighter/MainActivity.kt
+++ app/src/main/java/com/example/timefighter/MainActivity.kt
@@ -3,15 +3,16 @@ package com.example.timefighter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.CountDownTimer
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
-
+private val TAG = MainActivity::class.java.simpleName
 private var score = 0
 private var gameStarted : Boolean = false
 private lateinit var  countDownTimer: CountDownTimer
-private var initialCountDown: Long = 60000
+private var initialCountDown: Long = 6000
 private var countDownInterval: Long = 1000
 private var timeLeft = 60
 
@@ -25,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate called. Score is: $score")
 
         /// connect view to variables
         gameScoreTextView = findViewById(R.id.game_score_text_view)
@@ -67,6 +69,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFinish() {
                 //To be implemented later
+                endGame()
             }
         }
         gameStarted = false
@@ -80,5 +83,6 @@ class MainActivity : AppCompatActivity() {
 
     private  fun endGame(){
         //end game logic
+        Toast.makeText(this, getString(R.string.game_over_message, score), Toast.LENGTH_LONG).show()
     }
 }
